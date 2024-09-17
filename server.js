502ac38b20466047e2b0c97aef98690518d907f7
shahenoor radhanpuri <shahenooraslam29@gmail.com>
2024-09-16 23:06:06 -0600

    🗃️ [GR-3 BE-3]: Add user routes and link to server.js
    - Implemented user-related routes endpoints
    - Added the route path to `app.use` in `server.js` for handling user requests.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import knex from "knex";
 import config from "./knexfile.js"
 import "dotenv/config";
 import jobRoutes from './routes/jobRoutes.js'; 
-// import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const knexDb = knex(config.development);
 
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/jobs', jobRoutes); 
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
