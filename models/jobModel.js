a0eb3183a2915c69646d88b31d458b73e5395918
shahenoor radhanpuri <shahenooraslam29@gmail.com>
2024-09-16 21:57:17 -0600

    🗃️ [GR-8 BE-8]: Create job model with CRUD methods
    - Defined the schema and added methods for handling GET, POST, PUT, and DELETE operations in the job model.

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,26 +1,79 @@
-import axios from 'axios';
 import "dotenv/config";
 
-export const getJobById = ( req, id) => {
-    return req.knexDb("jobs").where({ id }).first();
+// Get a job with a specified job id
+export const getJobById = ( req, jobId) => {
+    return req.knexDb("jobs").where({ id: jobId }).first();
 }
 
-export const getJobsByMonth = ( req, id, month) => {
-    return req.knexDb("jobs").where({ user_id: id })
-    .andWhere(req.knexDb.raw('DATE_FORMAT(creation_date, "%Y-%m") = ?', [month])).select('*');
+// Get the total job count for a specified month
+export const getJobsByMonth = ( req, userId, month) => {
+    return req.knexDb("jobs").where({ user_id: userId })
+    .andWhere(req.knexDb.raw('DATE_FORMAT(creation_date, "%Y-%m") = ?', [month])).count();
 }
 
-export const getJobsByDay = ( req, id, date) => {
-    return req.knexDb("jobs").where({ user_id: id })
+// Get all the jobs for a specified date
+export const getJobsByDate = ( req, userId, date) => {
+    return req.knexDb("jobs").where({ user_id: userId })
     .andWhere(req.knexDb.raw('DATE_FORMAT(creation_date, "%Y-%m-%d") = ?', [date])).select('*');
 }
 
-export const getTotalJobApplications = ( req, id) => {
-    return req.knexDb("jobs").where({ user_id : id }).count();
+// Get count of Total Job Applied
+export const getTotalJobs = ( req, userId) => {
+    return req.knexDb("jobs").where({ user_id : userId }).count();
 }
 
+// Get all the jobs in Wishlist for a specified user
+export const getWishlistJobs = ( req, userId) => {
+    return req.knexDb("jobs").where({ user_id : userId })
+    .andWhere(({ status : 'wishlist' })).select('*');
+}
+
+// Create a new Job Application
 export const createJob = ( req, jobDetails) => {
+    const { id, title, company, location, description, applied_at, creation_date, deadline, color, salary, status, user_id } = jobDetails;
+    
+    if (!id || !title || !company || !location || !description || !applied_at || !creation_date || !deadline || !color || !salary || !status || !user_id) {
+        return res.status(400).send("Error: Missing properties");
+    }
     return req.knexDb("jobs").insert(jobDetails);
 }
 
+// Update Job Application
+export const updateJob = async ( req, res, jobDetails, jobId) => {
+    const { id, title, company, location, description, applied_at, deadline, color, salary, status } = jobDetails;
+    
+    if (!id || !title || !company || !location || !description || !applied_at || !deadline || !color || !salary || !status) {
+        return res.status(400).send("Error: Missing properties");
+    }
 
+    const job = await req.knexDb("jobs").where({ id: jobId }).first();
+    if (!job) {
+        return res.status(404).send('Job ID not found');
+    }
+
+    await req.knexDb("jobs").where({ id: jobId }).update({
+        id,
+        title,
+        company,
+        location,
+        description,
+        applied_at,
+        deadline,
+        color,
+        salary,
+        status,
+        updated_at: req.knexDb.fn.now(),
+    });
+    return req.knexDb("jobs").where({ id: jobId}).first();
+}
+
+// Delete Job Application 
+export const deleteJob = async ( req, res, jobId ) => {
+    const job = await req.knexDb("jobs").where({ id: jobId }).first();
+
+    if (!job) {
+        return res.status(404).send('Job ID not found');
+    }
+
+    return req.knexDb("jobs").where({ id: jobId }).del();
+}
