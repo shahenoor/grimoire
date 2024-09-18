a95b1eb06ed2bc76fc4810ce17bf2a6cf6824389
shahenoor radhanpuri <shahenooraslam29@gmail.com>
2024-09-18 15:40:46 -0600

    ✨ [GR-12 FE-4]: Implement monthly view with dynamic data styling
    - Added necessary packages from Material UI to support the monthly calendar.
    - Integrated Material UI library for mobile and tablet views.
    - Adjusted fonts and colors for a cohesive design.
    - Modified the tablet layout to match the desired structure and enhance responsiveness.
    - Added a favicon for the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import Header from './components/Header/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MonthlyView from './pages/MonthlyView/MonthlyView';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/calendar' element={<MonthlyView/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
