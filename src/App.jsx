18eae15a9c7155047c737d8162d96513f7fe9ad3
shahenoor radhanpuri <shahenooraslam29@gmail.com>
2024-09-19 17:24:30 -0600

    ✨ [GR-13 FE-8,9]: Implement kanban board for mobile and tablet with drag-and-drop
    - Created kanban board with all stages using `dnd-kit` for drag-and-drop functionality.
    - Added icons for each stage to enhance visual clarity.
    - Styled the board for both mobile and tablet views to match the overall app design and theme.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MonthlyView from './pages/MonthlyView/MonthlyView';
+import KanbanBoard from './pages/KanbanBoard/KanbanBoard';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/calendar' element={<MonthlyView/>}/>
+          <Route path='/board' element={<KanbanBoard/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/KanbanBoard/KanbanBoard.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { DndContext, useSensors, useSensor, MouseSensor, TouchSensor } from '@dnd-kit/core';
+import JobCard from '../../components/JobCard/JobCard';
+import KanbanColumn from '../../components/KanbanColumn/KanbanColumn';
+import add from '@assets/icons/add.svg';
+import './KanbanBoard.scss';
+
+function KanbanBoard() {
+    const containers = ['Wishlist', 'Applied', 'Interview', 'Offer', 'Rejection'];
+    const [items, setItems] = useState({
+      Wishlist: [],
+      Applied: [],
+      Interview: [],
+      Offer: [],
+      Rejection: []
+    });
+  
+    function handleDragEnd(event) {
+      const { active, over } = event;
+  
+      if (over) {
+        setItems((prev) => {
+          const newItems = { ...prev };
+          const draggedItem = newItems[active.data.current.parent].find(item => item.id === active.id);
+          newItems[active.data.current.parent] = newItems[active.data.current.parent].filter(item => item.id !== active.id);
+          newItems[over.id].push(draggedItem);
+          return newItems;
+        });
+      }
+    }
+    const sensors = useSensors(
+      useSensor(MouseSensor), 
+      useSensor(TouchSensor)  
+    );
+  
+    function addCard(container) {
+      const newId = `draggable-${Math.random().toString(36).substring(7)}`;
+      setItems((prev) => ({
+        ...prev,
+        [container]: [...prev[container], { id: newId, content: `New Job ${newId}` }]
+      }));
+    }  
+  
+    return (
+      <>
+      <DndContext  sensors={sensors} onDragEnd={handleDragEnd}>
+        <section className='board__outer'>
+          <div className='board__date-wrapper'>
+            <p className='board__date'>10th September 2024</p>
+          </div>
+          <div className='board__inner'>
+            {containers.map((id) => (
+              <KanbanColumn key={id} id={id} addCard = {addCard}>
+                {items[id].map((item) => (
+                  <JobCard key={item.id} id={item.id} parent={id}>
+                    {item.content}
+                  </JobCard>
+                ))}
+                {/* <button onClick={() => addCard(id)}>Add Card</button> */}
+              </KanbanColumn>
+            ))}
+          </div>
+        </section>
+      </DndContext>
+      <button className='board__float-button'>
+        <img src={add}/>
+      </button>
+    </>
+    );
+  }
+
+export default KanbanBoard
